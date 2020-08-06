6b046443d48fd85632aaf44bdce61b48f343f70e
Jay952 <jaydeep4420@gmail.com>
2020-08-06 23:28:27 +0530

    insert a node after a node pointed by pointer p is done

---
+++ insert_a_node_after_the_node_pointed_by_pointer_p.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+int main(){
+	int response;
+	cout<<"Type '1' to create a node\n";
+	cin>>response;
+	struct node *head, *z;
+	if(response == 1){
+		head = new node();
+		cout<<"Enter Head Data:";
+		cin>>head->data;
+		head->next = NULL;
+		z = head;
+	}
+	else{
+		cout<<"You have not typed 1";
+		return 0;
+	}
+    int n;
+	cout<<"The data in Head is:"<<head->data;
+	cout<<"\nNumber of nodes you want to insert after head (n>=4) =";
+	cin>>n;
+	int i=0;
+	while(i<n){
+		struct node *q;
+		q = new node();
+		cout<<"Enter the data of node "<<i+1<<":";
+		cin>>q->data;
+		z->next = q;
+		q->next = NULL;
+		z = z->next;
+		i++;
+	}
+	struct node *r;
+	r = head;
+	cout<<"Data of all nodes are respectively: ";
+	while(r != NULL){
+		cout<<r->data<<" ";
+		r = r->next;
+	}
+	
+	struct node *p;
+	p = head;
+	p = p->next->next->next;
+	cout<<"The node pointed by pointer p has data = "<<p->data;
+	cout<<"\nNow we are inserting a node after the node pointed by p\n";
+	
+	struct node *l;
+	l = new node();
+	cout<<"Enter data in new node which will be inserted after the node pointed by p\n";
+	cin>>l->data;
+	
+	l->next = p->next;
+	p->next = l;
+	cout<<"We have added a node after a node pointed by p\n";
+	
+	struct node *k;
+	k = head;
+	cout<<"The data of all the node after inserting a new node after a node pointed a pointer p\n";
+	while(k != NULL){
+		cout<<k->data<<" ";
+		k = k->next;
+	}
+	
+	
+	
+	return 0;
+}
