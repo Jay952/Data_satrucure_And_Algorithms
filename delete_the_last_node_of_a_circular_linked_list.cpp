54f41af97d0e9e3b4b40da7706b82a3b1679864a
Jay952 <jaydeep4420@gmail.com>
2020-08-07 21:21:16 +0530

    delete the last node of the circular linked list done

---
+++ delete_the_last_node_of_a_circular_linked_list.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+int main(){
+	int response;
+	cout<<"Type '1' to create a node\n";
+	cin>>response;
+	struct node *head, *p;
+	if(response == 1){
+		head = new node();
+		cout<<"Enter Head Data:";
+		cin>>head->data;
+		head->next = NULL;
+		p = head;
+	}
+	else{
+		cout<<"You have not typed 1";
+		return 0;
+	}
+    int n;
+	cout<<"The data in Head is:"<<head->data;
+	cout<<"\nNumber of nodes you want to insert after head (n) =";
+	cin>>n;
+	
+	int i=0;
+	while(i<n){
+		struct node *q;
+		q = new node();
+		cout<<"Enter the data of node "<<i+1<<":";
+		cin>>q->data;
+		p->next = q;
+		q->next = head;
+		p = p->next;
+		i++;
+	}
+	
+	struct node *r;
+	r = head;
+	cout<<"Data of all nodes in circular linked list are respectively, first and last data is of head node: ";
+	while(r->next != head){
+		cout<<r->data<<" ";
+		r = r->next;
+	}
+	cout<<r->data<<" ";
+	cout<<r->next->data;
+	
+	int response2;
+	
+	cout<<"\nEnter 1 if you want to delete the last node: ";
+	cin>>response2;
+	if(response2 == 1){
+		struct node *t;
+		t = head;
+		while(t->next->next != head){
+			t = t->next;
+		}
+		t->next = head;	
+	}
+	cout<<"Last node is deleted succesfully\n";
+	struct node *k;
+	k = head;
+	cout<<"Data of all nodes in circular linked list are respectively, first and last data is of head node: ";
+	while(k->next != head){
+		cout<<k->data<<" ";
+		k = k->next;
+	}
+	cout<<k->data<<" ";
+	cout<<k->next->data;
+	
+	return 0;
+}
